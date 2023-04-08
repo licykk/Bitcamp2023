16cafeae36929825bdf7f2c3ee8fe2237aeefc8b
licykk <licykk@gmail.com>
2023-04-08 07:46:39 -0400

    jk they changed some things in the library

--- bot.py
+++ bot.py
@@ -10,26 +10,19 @@ PREFIX = os.getenv('DISCORD_PREFIX')
 bot = commands.Bot(command_prefix=PREFIX)
 
 
-@bot.event
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
 async def on_ready():
-    guild = discord.utils.get(bot.guilds, name=GUILD)
-    print(
-        f'{bot.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print('We have logged in as ' + str(client.user))
 
-@bot.command(name='hi', help='responds with :flushed:')
-async def send_flushed(ctx):
-    response = ":flushed:"
-    await ctx.channel.send(response)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-
-if __name__ == "__main__":
-    # When running this file, if it is the 'main' file
-    # I.E its not being imported from another python file run this
-    for file in os.listdir("cogs"):
-        if file.endswith(".py") and not file.startswith("_"):
-            print(file)
-            bot.load_extension(f"cogs.{file[:-3]}")
-
-    bot.run(TOKEN)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
