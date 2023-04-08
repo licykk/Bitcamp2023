3507d5f5617f8549bf66fc2796411d107a07f771
licykk <licykk@gmail.com>
2023-04-08 13:45:29 -0400

    added cogs folder

---
+++ cogs/errorhandler.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+
+
+class ErrorHandler(commands.Cog):
+    """A cog for global error handling."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        """A global error handler cog."""
+
+        if isinstance(error, commands.CommandNotFound):
+            return  # Return because we don't want to show an error for every command not found
+        elif isinstance(error, commands.MissingPermissions):
+            message = "You are missing the required permissions to run this command!"
+        elif isinstance(error, commands.UserInputError):
+            message = "Something about your input was wrong, please check your input and try again!"
+        else:
+            message = "Oh no! Something went wrong while running the command!"
+
+        # await ctx.send(message, delete_after=3)
+        # await ctx.message.delete(delay=3)
+        await ctx.send(message)
+        await ctx.send(error)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(ErrorHandler(bot))
