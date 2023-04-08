103e41df1ea06844e7447e262a13b9f0553efa83
Jessica Lee <jlee1290@terpmail.umd.edu>
2023-04-08 18:54:39 -0400

    transaction working template

--- payments.py
+++ payments.py
@@ -6,7 +6,6 @@ import json
 import os
 
 API_KEY = os.getenv('CAPITALONE_API_KEY')
-print(API_KEY)
 URL_PATH = 'http://api.nessieisreal.com/'
 
 # Create customer, all arguments are strings
@@ -36,10 +35,9 @@ def create_customer(first_name, last_name, st_num, st_name, city, state, zip):
     else: #raise error later 
         print("No customer created")
 
-# Create account, all arguments are strings
+# Create account, all arguments are strings except rewards and balance are int
 def create_account(cust_id, act_type, nickname, rewards, balance, act_num):
     url = '{}customers/{}/accounts?key={}'.format(URL_PATH, cust_id, API_KEY)
-    print(url)
     payload = {
         "type": act_type,
         "nickname": nickname,
@@ -59,8 +57,33 @@ def create_account(cust_id, act_type, nickname, rewards, balance, act_num):
         print('account created')
     else: #raise error later 
         print("No account created")
-        print(response)
-        print(response.content)
 
 
-create_account('6431bb819683f20dd51877da', 'Credit Card', 'nickname', 10, 1000, '1234568890123456')
+# Initiate transaction, all arguments are strings
+# medium usually balance
+# all arguments are string except amt as int
+def create_transaction(src_act_id, medium, dest_act_id, status, description, amt):
+    url = '{}accounts/{}/transfers?key={}'.format(URL_PATH, src_act_id, API_KEY)
+    payload = {
+        "medium": medium,
+        "payee_id": dest_act_id,
+        "transaction_date": "2023-04-08",
+        "status": status,
+        "description": description,
+        "amount": amt
+    }
+
+    # Create an account request
+    response = requests.post( 
+        url, 
+        data=json.dumps(payload),
+        headers={'content-type':'application/json'},
+        )
+
+    if response.status_code == 201:
+        print('transaction created')
+    else: #raise error later 
+        print("No transaction created")
+
+
+create_transaction('6431e0c09683f20dd51877eb', 'balance', '6431ca439683f20dd51877e2', 'pending', 'description', 500)
