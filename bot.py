adb37ec948103e5bb549da9966f957ad5649db88
Eric Yi <ericyi24@terpmail.umd.edu>
2023-04-09 05:52:20 -0400

    Update to interactions

--- bot.py
+++ bot.py
@@ -42,26 +42,6 @@ async def load_extensions():
             # cut off the .py from the file name
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
-
-@bot.tree.command(description="Gets information on a user. Defaults to the user who ran the command.")
-async def whois(interaction: discord.Interaction, member: discord.Member=None):
-    if member is None:
-        member = interaction.user
-    embed = discord.Embed(title="User Info", description=f"Here's the user information for {member}", color=discord.Color.blue(), timestamp=datetime.datetime.utcnow())
-    embed.set_thumbnail(url=member.avatar)
-    embed.add_field(name="User ID", value=member.id)
-    embed.add_field(name="User Name", value=f'{member.name}#{member.discriminator}')
-    embed.add_field(name="Nickname", value=member.display_name)
-    await interaction.response.send_message(embed=embed)
-
-
-@bot.tree.command(description="Requests a user to pay a certain amount of money to you.")
-async def requestpayment(interaction: discord.Interaction, member: discord.Member, amount: int, comment: str=None):
-    #embed: discord.Embed = discord.Embed(title="Payment Request", description=f"{interaction.user.mention} has requested ${amount} from {member.mention}", color=discord.Color.blue(), timestamp=datetime.datetime.utcnow())
-    if comment is None:
-        await interaction.response.send_message(content=f'{interaction.user.mention} has requested ${amount} from {interaction.user.mention}.')
-    await interaction.response.send_message(content=f'{interaction.user.mention} has requested ${amount} from {interaction.user.mention}. \n Comments: {comment}')
-
 async def main():
     async with bot:
         await load_extensions()
